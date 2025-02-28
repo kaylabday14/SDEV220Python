e6e01c86437029e5a4e6622a8a71e71b5984ab4d
Your Name <kaylabday14@gmail.com>
2025-02-28 13:10:30 -0500

    Django tutorials

--- djangogirls/blog/views.py
+++ djangogirls/blog/views.py
@@ -1,9 +1,12 @@
 # blogs folder
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from datetime import datetime, timezone, timedelta
+from django.utils.timezone import now
 from .models import Post
-
+from .forms import PostForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 # Create your views here.
 
 def post_list(request):
@@ -12,3 +15,39 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=time_now).order_by('published_date')
 
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+@login_required
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+@login_required
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
--- djangogirls/mysite/settings.py
+++ djangogirls/mysite/settings.py
@@ -116,10 +116,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Security and Authorization
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
